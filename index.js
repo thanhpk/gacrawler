0211949f738f2fb700d3f31a34dc38e0cac55598
Pham Kieu Thanh <thanhpk@live.com>
2016-09-08 15:16:29 +0700

    retry when limit exceed

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function GaCrawler(viewId, clientEmail, privateKey) {
 		});
 	}
 
-	function queryPageviewAndUniqueVisitor(url, startdate, enddate, nTry) {
+	function queryPageview(url, startdate, enddate, nTry) {
 		var deferred = Q.defer();
 		lock('auth', function(release){
 			release()();
@@ -46,9 +46,18 @@ function GaCrawler(viewId, clientEmail, privateKey) {
 				'filters': 'ga:pagePath==' + url
 			}, function (err, response) {
 				if (err) {
+					if (err.code === 403 && err.errors && err.errors[0] && err.errors[0].reason === 'userRateLimitExceeded') {
+						// keep try again
+						return queryPageview(url, startdate, enddate)
+						.then(function(data){
+							deferred.resolve(data);
+						}).catch(function(data){
+							deferred.reject(data);
+						});
+					}
 					if (nTry === undefined) {
 						// try to authenticate again
-						queryPageviewAndUniqueVisitor(url, startdate, enddate, 1)
+						return queryPageview(url, startdate, enddate, 1)
 						.then(function(data){
 							deferred.resolve(data);
 						}).catch(function(data){
@@ -59,28 +68,80 @@ function GaCrawler(viewId, clientEmail, privateKey) {
 					}
 				}
 				var pageViews = response.totalsForAllResults['ga:pageViews'];
-				analytics.data.ga.get({
-					'auth': jwtClient,
-					'ids': viewId,
-					'metrics': 'ga:Users',
-					'dimensions': 'ga:pagePath',
-					'start-date': startdate,
-					'end-date': enddate,
-					'filters': 'ga:pagePath==' + url
-				}, function (err, response) {
-					if (err) {
-						return deferred.reject(err);
-					}
-					var uniqueUsers = response.totalsForAllResults['ga:Users'];
-					deferred.resolve({totalPageviews: pageViews, totalUniqueUsers: uniqueUsers});
-				});
+				deferred.resolve(pageViews);
 			});
 		});
 		return deferred.promise;
 	}
 
+	function queryUniqueVisitor(url, startdate, enddate, nTry) {
+		var deferred = Q.defer();
+		lock('auth', function(release){
+			release()();
+			if (!jwtClient)
+				return deferred.reject('wrong access');
+
+			analytics.data.ga.get({
+				'auth': jwtClient,
+				'ids': viewId,
+				'metrics': 'ga:Users',
+				'dimensions': 'ga:pagePath',
+				'start-date': startdate,
+				'end-date': enddate,
+				'filters': 'ga:pagePath==' + url
+			}, function (err, response) {
+				if (err) {
+					if (err.code === 403 && err.errors && err.errors[0] && err.errors[0].reason === 'userRateLimitExceeded') {
+						// keep try again
+						return queryUniqueVisitor(url, startdate, enddate)
+						.then(function(data){
+							deferred.resolve(data);
+						}).catch(function(data){
+							deferred.reject(data);
+						});
+					}
+					if (nTry === undefined) {
+						// try to authenticate again
+						return queryUniqueVisitor(url, startdate, enddate, 1)
+						.then(function(data){
+							deferred.resolve(data);
+						}).catch(function(data){
+							deferred.reject(data);
+						});
+					} else {
+						return deferred.reject(err);
+					}
+				}
+					var uniqueUsers = response.totalsForAllResults['ga:Users'];
+					deferred.resolve(uniqueUsers);
+			});
+		});
+		return deferred.promise;
+	}
+
+	function queryPageviewAndUniqueVisitor(url, startdate, enddate, nTry) {
+		var deferred = Q.defer();
+		queryPageview(url, startdate, enddate).then(function(pageViews){
+			queryUniqueVisitor(url, startdate, enddate).then(function(uniqueVisitors){
+				deferred.resolve({totalPageviews: pageViews, totalUniqueUsers: uniqueVisitors});
+			}).catch(function(err){
+				deferred.reject(err);
+			});
+		}).catch(function(err){
+			deferred.reject(err);
+		});
+
+		return deferred.promise;
+	}
+
+	function batchQueryPageviewAndUniqueVisitor(urls, startdate, enddate, nTry) {
+		
+	}
+
 	auth(clientEmail, privateKey);
 	this.queryPageviewAndUniqueVisitors = queryPageviewAndUniqueVisitor;
+
+	this.batchQueryPageviewAndUniqueVisitors = batchQueryPageviewAndUniqueVisitor;
 }
 
 exports.gaCrawler = GaCrawler;
